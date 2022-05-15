e0da504ae25f111b5a69d29ec492666dca3467ff
NetanelYaso <netanelyaso12@gmail.com>
2022-05-15 18:37:19 +0300

    homework

---
+++ main.js
@@ -0,0 +1,159 @@
+//  function printMycontry(){
+//      console.log('ISRAEL')
+//  }
+//  printMycontry()
+
+// function printMyname(){
+//     console.log('Netanel Yaso')
+// }
+// printMyname()
+
+// function printNumber(){
+//     console.log(7)
+// }
+// printNumber()
+
+// function printFivenumbers(){
+// for(i=0; i<5; i++){
+//     console.log(3)
+// }
+// }
+// printFivenumbers()
+// function printAvgAndSum(){
+// var i=4
+// var num=6
+// var number=2
+// var x=4
+// var sum= i+num+number+x
+// console.log(sum)
+// var avg=sum/4
+// console.log(avg)
+// }
+// printAvgAndSum()
+
+// function consoleArea(){
+// var hight=10
+// var width=10
+// var area= hight*width
+// console.log(area)
+// }
+// consoleArea()
+
+// function switchNum(num1,num2){
+// console.log(num1,num2)
+// var num3=0
+// num3=num1
+// num1=num2
+// num2=num3
+// console.log(num1,num2)
+// }
+// switchNum(24,23)
+
+// function numAndavg(){
+// var i = +prompt('enter first number')
+// var num = +prompt('enter second number')
+// var number = +prompt('enter third number')
+// var x = +prompt('enter forth number')
+// var sum = i+num+number+x
+// console.log(sum)
+// var avg= sum/4
+// console.log(avg)
+// }
+// numAndavg()
+
+// var hight = +prompt('enter 1 number')
+// var width = +prompt('enter 2 number')
+// var area = hight*width
+// console.log(area)
+
+// function swiutchNumbers(){
+// var num1 = +prompt('enter 1 number')
+// var num2 = +prompt('enter 2 number')
+// var num3 =0
+// console.log(num1,num2)
+// num3=num1
+// num1=num2
+// num2=num3
+// console.log(num1,num2)
+// }
+// swiutchNumbers()
+
+// function printTwonumbers(){
+// var num1= +prompt('enter first number')
+// var num2= +prompt('enter second number')
+// console.log(num1,num2)
+// }
+// printTwonumbers()
+
+// function assigningNum(){
+// var a1 ='a'
+// var a2 ='1'
+// console.log(a1,a2)
+// }
+// assigningNum()
+
+//פה צריכים להיות משתנים ואופרטורים
+
+// function promptNum(){
+// var num = +prompt('enter a number')
+// if(num>10){
+//     console.log('greater than 10')
+// }
+// }
+// promptNum()
+
+// function ifNumten(){
+// var num = +prompt('put a number')
+// if(num<10){
+//     console.log('10 greater then')
+// }
+// else if(num>10){
+//     console.log('10 less then')
+// }
+// else{
+//     console.log('equal to 10')
+// }
+// }
+// ifNumten()
+
+// function sizesOfvalues(){
+// var first= +prompt('enter first number')
+// var second= +prompt('enter second number')
+// if(first>second){
+//     console.log(first)
+// }
+// else if(first<second){
+//     console.log(second)
+// }
+// else{
+//     console.log('values are equal')
+// }
+// }
+// sizesOfvalues()
+// function numbersUntilhundred() {
+    
+
+// var i =1
+// while(i<101){
+//     console.log(i)
+//     i++
+// } 
+// }
+// numbersUntilhundred()
+// function avgAndsum(){
+//  var num = 0;
+//  var sum = 0;
+//  while(num<5){
+//  var number = +prompt('sign your number')
+//      sum += number
+//      num++
+    
+//      if(num % 2 == 0){
+//         var avg = sum /num 
+//          }
+//      }
+//      console.log(sum)
+//      console.log(avg)
+//  }
+//  avgAndsum()
+
